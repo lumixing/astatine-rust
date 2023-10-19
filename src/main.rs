7a078cf518df1d0deb648d0b1eb606b556f90e1b
= <=>
2023-10-19 08:12:59 +0300

    initalization v2

--- src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let mut app = astatine::app();
-    app.run();
-}
--- src/states.rs
+++
@@ -1,9 +0,0 @@
-use bevy::prelude::States;
-
-#[derive(Clone, Copy, Eq, PartialEq, Debug, Hash, Default, States)]
-pub enum GameState {
-    #[default]
-    AssetLoading,
-    WorldGeneration,
-    InGame,
-}
